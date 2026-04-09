0a0da847e0370fa0c75896a4e54b5f5684739e2c
Samuel Manzanera <samuelmanzanera@protonmail.com>
2026-04-09 16:13:21 +0200

    connect the front with the backend

--- web/src/components/GetStarted.tsx
+++ web/src/components/GetStarted.tsx
@@ -1,14 +1,23 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Copy, Check } from "lucide-react";
 import { Button } from "./ui/button";
 import { toast } from "sonner";
 import { SectionHeader } from "./SectionHeader";
+import { relayInfo } from "@/lib/nostr";
 
-const PUBKEY = "npub1sigr4te7f8wq3kx8ahj29vm5ndcl4pz6y20fw0ghst7e5j8mplqx8q2k";
 const NPM_CMD = "npm install @pricestr/sdk";
 
 // export default GetStarted;
 const GetStarted = () => {
+
+  const [relayPub, setRelayPub] = useState("")
+
+  useEffect(() => {
+    relayInfo().then(info => {
+      console.log(info)
+      setRelayPub(info.pubkey)
+    })
+  }, [])
 
   const handleCopy = (value) => {
     navigator.clipboard.writeText(value);
@@ -16,7 +25,7 @@ const GetStarted = () => {
   };
 
   return (
-    <section className="pb-40 ">
+    <section className="pb-40" id="get-started">
       <div className="max-w-7xl mx-auto px-6 flex flex-col gap-10">
         <SectionHeader title="Integration" />
         <header className="flex justify-between items-center gap-5">
@@ -24,24 +33,26 @@ const GetStarted = () => {
             Two lines<br /><span className="text-violet-400">to go</span>
           </h2>
         </header>
-        <div className="flex">
-          <div className="border border-border/10 p-10 flex flex-col gap-5 w-1/2">
+        <div className="flex flex-col md:flex-row gap-10">
+          <div className="border border-border/10 flex flex-col gap-5 lg:w-1/2">
             <span className="font-mono tracking-widest text-primary text-xs font-bold uppercase">Option a</span>
             <h3 className="tracking-tight font-[900] font-title uppercase text-4xl">Subscribe via Nostr</h3>
             <div className="flex flex-col gap-1">
               <p className="text-muted-foreground/80 uppercase font-mono text-xs ">Public key</p>
-              <div className="flex">
-                <p className="text-xs flex-1 p-2 border flex items-center bg-white/5">{PUBKEY}</p>
-                <Button variant='outline' className="px-5 py-2 text-xs" onClick={() => handleCopy(PUBKEY)}>Copy</Button>
+              <div className="flex flex-col md:flex-row">
+                <p className="text-xs flex-1 p-2 border flex items-center bg-white/5 break-all">
+                  {relayPub}
+                </p>
+                <Button variant='outline' className="flex px-5 py-2 text-xs" onClick={() => handleCopy(relayPub)}>Copy</Button>
               </div>
             </div>
           </div>
-          <div className="border border-l-0 border-border/10 p-10 flex flex-col gap-5 w-1/2">
+          <div className="border border-l-0 border-border/10 flex flex-col gap-5 lg:w-1/2">
             <span className="font-mono tracking-widest text-primary text-xs font-bold uppercase">Option b</span>
             <h3 className="tracking-tight font-[900] font-title uppercase text-4xl">Use the SDK</h3>
             <div className="flex flex-col gap-1">
               <p className="text-muted-foreground/80 uppercase font-mono text-xs ">Install</p>
-              <div className="flex">
+              <div className="flex flex-col md:flex-row">
                 <p className="text-xs flex-1 p-2 border flex items-center bg-white/5">{NPM_CMD}</p>
                 <Button variant='outline' className="px-5 py-2 text-xs" onClick={() => handleCopy(NPM_CMD)}>Copy</Button>
               </div>
--- web/src/components/Hero.tsx
+++ web/src/components/Hero.tsx
@@ -1,34 +1,55 @@
 import { useEffect, useState } from "react";
 import { Button } from "./ui/button";
+import { lastPrices, PriceData, subscribePrice } from "@/lib/nostr";
+
+import { Line, LineChart, YAxis } from "recharts"
+import { ChartConfig, ChartContainer, ChartTooltip, ChartTooltipContent } from "./ui/chart";
+import { Link } from "react-router-dom";
 
 const Hero = () => {
-  const [showWave, setShowWave] = useState(false);
+  const [chartData, setChartData] = useState<PriceData[]>([]);
+  const [changePrices, setChangePrices] = useState([])
+  const chartConfig = {} satisfies ChartConfig
 
   useEffect(() => {
-    setShowWave(true);
+    lastPrices().then((data) => {
+      setChartData(data)
+      setChangePrices(data.map(d => d.aggregate).slice(0, 2))
+
+      subscribePrice((data) => {
+        setChartData(prev => [...prev, data]);
+        setChangePrices(prev => {
+          if (prev.length == 0) return [data.aggregate]
+          if (prev.length == 1) return [data.aggregate, prev]
+          return [data.aggregate, prev[0]]
+        })
+      })
+    })
   }, []);
 
   return (
     <section className="relative min-h-screen flex items-center overflow-hidden">
-      <div className="flex justify-between max-w-7xl mx-auto px-6">
-        <div className="w-1/2 relative z-10 flex flex-col gap-5 ">
-        <span className="text-lg">Signed <span className="text-primary">Bitcoin</span> price feed for Nostr</span>
+      <div className="flex flex-col md:flex-row justify-between w-full p-5 md:px-60 gap-20">
+        <div className="relative z-10 flex flex-col gap-5 md:w-1/3">
           <h1 className="text-6xl md:text-8xl tracking-tight mb-6 font-[900] font-title uppercase">
             <span className="text-white">Price<span className="text-violet-500">Str</span></span>
           </h1>
+          <h2 className="text-lg">Signed <span className="text-primary">Bitcoin</span> price feed for Nostr</h2>
           <div className="border-l border-primary pl-5 flex flex-col gap-1 font-mono uppercase tracking-tight text-left text-xs">
-            <p>Cryptographically signed</p>
             <p>Relay-distributed</p>
             <p>Publicly auditable</p>
+            <p>Cryptographically signed</p>
           </div>
           <div className="flex gap-5 mt-10">
-            <Button className="bg-white">View pubkey</Button>
-            <Button variant="outline">Read the docs</Button>
+            <Button className="bg-white w-full" asChild>
+              <Link to='#get-started'>View pubkey</Link>
+            </Button>
+            <Button variant="outline" className="w-full">Read the docs</Button>
           </div>
         </div>
 
-        <div className="flex w-1/2 flex-col font-mono uppercase text-xs">
-          <div className="border border-white/10 flex justify-between p-3 ">
+        <div className="flex flex-col font-mono text-xs md:w-2/3">
+          <div className="border border-white/10 flex justify-between p-3 uppercase">
             <span>Price signal</span>
             <span className="text-primary flex items-center gap-2">
               <span className="relative flex size-3">
@@ -37,25 +58,67 @@ const Hero = () => {
               </span>
               Live
             </span>
-            <span>10s interval</span>
+            <span>5s interval</span>
           </div>
-          <div className="border border-t-0 border-white/10 ">
-            <canvas height={251} width={899}></canvas>
+          <div className="border border-t-0 border-white/10">
+            {/* <canvas height={251} width={899} id="price-chart"></canvas> */}
+            <ChartContainer config={chartConfig}>
+              <LineChart
+                accessibilityLayer
+                data={chartData}
+              >
+                <ChartTooltip
+                  cursor={true}
+                  content={<ChartTooltipContent  className="w-[200px]" indicator='line' />}
+                />
+                <YAxis domain={['dataMin - 100', 'dataMax + 100']} hide={true} />
+                {Object.entries(chartData[0]?.sources || {}).map(([source, _]) => (
+                  <Line
+                  key={source}
+                  dataKey={`sources.${source}`}
+                  type="natural"
+                  stroke={source === 'binance' ? '#ffdd00' : source === 'coinbase' ? '#0059ff' : '#6532b7'}
+                  dot={false}
+                  opacity={0.2}
+                  strokeWidth={1}
+                  name={source}
+                  />
+                ))}
+                <Line
+                  dataKey="aggregate"
+                  type="natural"
+                  stroke="#ff9d00"
+                  strokeWidth={2}
+                  dot={false}
+                />
+              </LineChart>
+            </ChartContainer>
           </div>
           <div className="border border-t-0 border-white/10 flex justify-between">
             <div className="flex-1 flex flex-col p-5 gap-2 border-r border-white/10">
               <div className="flex justify-between ">
                 <span className="">BTC / USD</span>
-                <span className="text-red-500">0.04</span>
+                {changePrices.length > 1 && (changePrices[0] - changePrices[1]) != 0 && <span className={`${changePrices[0] - changePrices[1] < 0 ? 'text-red-500' : 'text-green-500'}`}>
+                  {new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(changePrices[0] - changePrices[1])}
+                </span>}
               </div>
-              <p className="font-title text-3xl font-bold">97,573.32</p>
+              {chartData.length > 0 &&
+                <p className="font-title text-3xl font-bold">{new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(changePrices[0])}</p>
+              }
+              <p className="text-xs text-gray-500">median</p>
             </div>
             <div className="flex-1 flex flex-col p-5 gap-2">
-              <div className="flex justify-between">
-                <span className="">BTC / USD</span>
-                <span className="text-red-500">0.04</span>
+              <div className="flex justify-between uppercase">
+                <span className="">Sources</span>
               </div>
-              <p className="font-title text-3xl font-bold">97,573.32</p>
+              <div className="flex flex-col mt-2">
+                {chartData.length > 0 && Object.entries(chartData[chartData.length - 1].sources).map(([source, price]) => (
+                  <div key={source} className="text-sm font-mono uppercase tracking-tight flex items-center gap-2">
+                    <div className={`w-2 h-2 rounded-full ${source === 'binance' ? 'bg-yellow-400/40' : source === 'coinbase' ? 'bg-blue-600/40' : 'bg-violet-800/40'}`}></div>
+                    {source}: {new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(price)}
+                  </div>
+                ))}
+              </div>
             </div>
           </div>
         </div>
