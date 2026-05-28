305316fbc043dbce8a827e50c75f20373c2a0e76
Samuel Manzanera <samuelmanzanera@protonmail.com>
2026-05-28 10:41:19 +0200

    fix: fix posthog config

--- src/main.tsx
+++ src/main.tsx
@@ -5,8 +5,9 @@ import posthog from "posthog-js";
 import { PostHogProvider } from "@posthog/react";
 
 posthog.init(import.meta.env.VITE_PUBLIC_POSTHOG_PROJECT_TOKEN, {
+  api_host: import.meta.env.VITE_PUBLIC_POSTHOG_HOST,
   ui_host: 'https://eu.posthog.com', // necessary because you're using a proxy, this way links will point back to PostHog properly
-  defaults: "2026-01-30",
+  defaults: '2026-01-30',
 });
 
 createRoot(document.getElementById("root")!).render(
